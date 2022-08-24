9120df8a5a7f56613737ddf604f532546e5ebd1b
Felix Grzelka <felix.grzelka@hpi.de>
2022-08-24 17:54:25 +0200

    fix mem_fence on x86 for local testing

--- smog.h
+++ smog.h
@@ -14,23 +14,29 @@ extern size_t g_smog_timeout;
 extern bool g_measuring;
 extern pthread_barrier_t g_initalization_finished;
 
-#define mem_fence()  __asm__ __volatile__ ("lwsync" : : : "memory")
+#ifdef __powerpc64__
+    #define mem_fence()  __asm__ __volatile__ ("lwsync" : : : "memory")
+#endif
+
+#ifdef __x86_64__
+    #define mem_fence()  __asm__ __volatile__ ("mfence" : : : "memory")
+#endif
 
 struct __attribute__((packed)) thread_status_t {
         size_t count;
-	char padding[CACHE_LINE_SIZE - sizeof(size_t)];
+    char padding[CACHE_LINE_SIZE - sizeof(size_t)];
 };
 
 extern struct thread_status_t *g_thread_status;
 
 class Thread_Options {
-        public:
-                Thread_Options(int tid, size_t page_count, void *page_buffer) :
-                        tid(tid), page_count(page_count), page_buffer(page_buffer)
-                {}
-                int tid;
-                size_t page_count;
-                void *page_buffer;
+    public:
+        Thread_Options(int tid, size_t page_count, void *page_buffer) :
+            tid(tid), page_count(page_count), page_buffer(page_buffer)
+        {}
+        int tid;
+        size_t page_count;
+        void *page_buffer;
 };
 
 long double get_unixtime();
